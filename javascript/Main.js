81261dc52820df0aee55fe2c4997dcad301ca19e
AnonGhostID <adamakbar26@gmail.com>
2022-11-17 10:44:02 +0700

    donationform lama

--- javascript/Main.js
+++ javascript/Main.js
@@ -74,7 +74,14 @@ loginForm.addEventListener('submit', (login) => {
 /*
     ----------------------------Proses Validating Halaman Donation Form---------------------------
 */
+const donateForm = document.getElementById('sbmt').addEventListener("click", function(subb){
 
+    subb.preventDefault();
+
+    alert('halo bang')
+});
+    
+    
 /*
     ----------------------------Proses Validating Halaman Driver Form---------------------------
 */
