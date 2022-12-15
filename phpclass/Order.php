02c5920aa91304179cea13fe803d15218c48c7bd
fidiramadhina <9hwahyufidi23@gmail.com>
2022-12-15 09:35:18 +0700

    order php

---
+++ phpclass/Order.php
@@ -0,0 +1,14 @@
+<?php
+class Order{
+    public $resto;
+    private $pesanan;
+    private $jumlah;
+    private $total;
+    protected $bayar;
+    private $alamat;
+
+    function order(){
+        echo 'order';
+    }
+    
+}
