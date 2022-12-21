49d895c19a5951a85967bc54841310e4192a581e
AnonGhostID <adamakbar26@gmail.com>
2022-12-21 21:38:08 +0700

    -

---
+++ php/prosesDonation.php
@@ -0,0 +1,17 @@
+<?php
+
+$nama = $_POST['namadonatur'];
+$email = $_POST['emailform'];
+$feedback = $_POST['feedback'];
+$nohp = $_POST['nohp'];
+$jumdon = $_POST['jumlahdonasi'];
+$metodepmb = $_POST['paymeth'];
+
+echo $nama."</br>";
+echo $email."</br>";
+echo $feedback."</br>";
+echo $nohp."</br>";
+echo $jumdon."</br>";
+echo $metodepmb."</br>";
+
+?>
